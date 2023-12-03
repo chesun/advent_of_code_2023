0186814f80ef22daefe0c05e0f85559928773e4e
Christina Che Sun <che.sun.1996@gmail.com>
2023-12-02 22:44:41 -0800

    add day2 part 2

--- day2/day2_solution.py
+++ day2/day2_solution.py
@@ -5,6 +5,7 @@
 all_colors = ["red", "green", "blue"]
 bag = {"red": 12, "green": 13, "blue": 14}
 sum_possible_ids = 0
+sum_power = 0
 
 with open("./day2/input.txt", "r") as file:
     lines = file.readlines()
@@ -27,8 +28,11 @@ with open("./day2/input.txt", "r") as file:
         rounds_str = line.split(":")[1].strip()
         rounds = rounds_str.split(";")
 
-        # convert each round into a dictionary
+        # a list to hold all rounds as strings
         rounds_cleaned = []
+
+        # list of values for the number of cubes for each color
+        color_numbers = {color : [] for color in all_colors}
         # looping over rounds in the current game
         for round_str in rounds:
             # list for the round
@@ -49,18 +53,18 @@ with open("./day2/input.txt", "r") as file:
             # print(round_dict)
             # check if this round is possible
             for color in all_colors:
+                color_numbers[color].append(round_dict[color])
                 if round_dict[color] > bag[color]:
                     game_possible = False
-        
-        
+        # print(color_numbers)
+        # calculate the power of the minimum possible numbers
+        power_game = max(color_numbers["red"]) * max(color_numbers["green"]) * max(color_numbers["blue"])
+        sum_power += power_game
+
         if game_possible is True:
             sum_possible_ids += game_id
 
-          
-
-        
-
-    print(sum_possible_ids)
+    print(sum_possible_ids, sum_power)
 
 
 
